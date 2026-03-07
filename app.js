acdca34739aad5c49ac017084549347b4c295e7d
Alexandre François <alexandre.francois0901@gmail.com>
2026-03-07 22:26:51 +0100

    Logique métier - analyse leads et éligibilité MPR

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const leads = [
-  {
-    nom: "Martin Dupont",
-    adresse: "12 rue de la Thur, Thann",
-    telephone: "06 12 34 56 78",
-    estProprietaire: true
-  },
-  {
-    nom: "Sophie Bernard",
-    adresse: "8 avenue d'Alsace, Mulhouse",
-    telephone: "06 98 76 54 32",
-    estProprietaire: false
+function analyserLead(lead) {
+  console.log("--- Analyse du lead ---")
+  console.log(`Nom : ${lead.nom}`)
+  console.log(`Adresse :  ${lead.adresse}`)
+
+  if (!lead.estProprietaire) {
+    console.log("Non éligible aux aides - locataire")
+    return
   }
+
+  if (lead.revenuFiscal < 15000) {
+    console.log("Taux MPR : 50%")
+  } else if (lead.revenuFiscal < 30000) {
+    console.log("Taux MPR : 40%")
+  } else if (lead.revenuFiscal < 45000) {
+    console.log("Taux MPR : 30%")
+  } else {
+    console.log("Taux MPR : 15%")
+  }
+}
+
+const leads = [
+  { nom: "Martin Dupont", adresse: "12 rue de la Thur, Thann", estProprietaire: true, revenuFiscal: 25000 },
+  { nom: "Sophie Bernard", adresse: "8 avenue d'Alsace, Mulhouse", estProprietaire: false, revenuFiscal: 18000 },
+  { nom: "Pierre Martin", adresse: "3 rue de Cernay, Mulhouse", estProprietaire: true, revenuFiscal: 12000 }
 ]
 
 leads.forEach(function(lead) {
-  console.log(lead.nom + " - " + lead.adresse)
+  analyserLead(lead)
 })
