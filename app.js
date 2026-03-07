da7e7c7bbe8788139f919918352c87cce5741889
Alexandre François <alexandre.francois0901@gmail.com>
2026-03-07 22:42:44 +0100

    Intégration API Adresse data.gouv.fr

--- app.js
+++ app.js
@@ -2,15 +2,31 @@ const bouton = document.getElementById("boutonSimuler")
 const champAdresse = document.getElementById("adresse")
 const resultat = document.getElementById("resultat")
 
-function analyserAdresse(adresse) {
-  if (adresse === "") {
-    return "Veuillez entrer une adresse."
-  }
-  return `Analyse en cours pour : ${adresse}`
-}
-
 bouton.addEventListener("click", function() {
   const adresse = champAdresse.value
-  const message = analyserAdresse(adresse)
-  resultat.textContent = message
+
+  if (adresse === "") {
+    resultat.textContent = "Veuillez entrer une adresse."
+    return
+  }
+
+  const url = `https://api-adresse.data.gouv.fr/search/?q=${encodeURIComponent(adresse)}&limit=1`
+
+  fetch(url)
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(data) {
+  const premier = data.features[0]
+  const label = premier.properties.label
+  const ville = premier.properties.city
+  const codePostal = premier.properties.postcode
+  const coords = premier.geometry.coordinates
+
+  resultat.textContent = `
+    Adresse : ${label}
+    Ville : ${ville} (${codePostal})
+    Coordonnées : ${coords[1]}, ${coords[0]}
+  `
+})
 })
